c7a514dcbb2356e89521dab8eb8e8749d1dd1855
kailin <kailin.tan.2010@gmail.com>
2022-01-13 00:20:48 +0800

    wip

--- week4-language-and-compilers/calculator/scanner.js
+++ week4-language-and-compilers/calculator/scanner.js
@@ -5,22 +5,36 @@
 //     (PLUS, "+"),
 //     (RIGHT_PAREN, ")"
 // ]
+const BLANK_CHARACTERS = ['', '\s', '\t'];
 
 class Token {
   constructor(character) {
     this.character = character;
   }
   computeType() {
-    if (!isNaN(this.character)) { return 'NUMBER'; }
-    switch(this.character) {
+    let trimmedCharacter = this.character.trim();
+    if (!isNaN(trimmedCharacter) && !BLANK_CHARACTERS.includes(trimmedCharacter)) {
+      return 'NUMBER';
+    }
+    switch(trimmedCharacter) {
       case '+':
         return 'PLUS';
       case '-':
         return 'MINUS';
+      case '*':
+        return 'MULTIPLY';
+      case '/':
+        return 'DIVIDE';
+      case '^':
+        return 'EXPONENT';
       case '(':
         return 'LEFT_PAREN';
       case ')':
         return 'RIGHT_PAREN';
+      case '':
+      case '\r':
+      case '\t':
+        return 'SPACE';
       default:
         return 'INVALID_CHAR';
     }
@@ -31,9 +45,10 @@ class Token {
 }
 
 exports.scanner = function(calculatorString) {
+  let characterArray = calculatorString.split('');
   let tokens = [];
-  for (var i = 0; i <= calculatorString.length; i++) {
-    tokens.push(new Token(calculatorString.charAt(i)));
+  while(characterArray.length) {
+    tokens.push(new Token(characterArray.shift()));
   }
   tokens.forEach((t) => {
     t.printToken();
